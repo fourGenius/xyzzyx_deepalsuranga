d8273fad26b05259e1505e2951de5a55d7634dae
Dineth Wasuka Jayasekera <dinethwa@gmail.com>
2016-01-03 02:30:35 +0530

    create employee module

---
+++ src/com/fourgenius/www/public_access/employee/model/employee.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fourgenius.www.public_access.employee.model;
+
+/**
+ *
+ * @author Dineth Jayasekera
+ */
+public class employee {
+    /*
+    Table Names :
+    employee_academic_user_info
+    employee_academic_user_recovery
+    employee_academic_user_status
+    employee_academic_user_info_name
+    employee_academic_user_info_contact
+    employee_academic_user_info_personal
+    employee_academic_user_info_qulifications
+    employee_academic_user_info_address
+    employee_academic_user_info_qulifications
+    employee_nonAcademic_administrative_user_info
+    employee_nonAcademic_administrative_user_recovery
+    employee_nonAcademic_administrative_user_status
+    
+    Name:
+    String 
+    
+    */
+}
