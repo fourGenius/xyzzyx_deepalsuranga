f66156ca39048540783c1914068729ab4b5e5946
Deepal Suranga Weerasuriya <deepal.suranga29@gmail.com>
2016-02-10 17:17:03 +0530

    Create Package account

---
+++ src/com/fourgenius/www/public_access/payroll/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fourgenius.www.public_access.payroll;
+
+/**
+ *
+ * @author deepalsuranga
+ */
+public class NewClass {
+    
+}
